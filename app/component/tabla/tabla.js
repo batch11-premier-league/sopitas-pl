d1335a5e1fe24a9808906693e65dad8c48a4fefb
dircdirc <dircdirc@gmail.com>
2016-10-05 23:18:45 -0500

    Tabla_Posiciones
    Tabla de posiciones

---
+++ app/component/tabla/tabla.js
@@ -0,0 +1,26 @@
+(function () {
+	'use strict';
+
+	var tablasList = {
+		templateUrl: './app/component/tabla/tabla.html',
+		controller: tablasListCtrl
+	}
+
+	angular
+		.module('sopitasApp')
+		.component('tablasList', tablasList);
+
+	tablasListCtrl.$inject = ['apiTabla']
+	function tablasListCtrl (apiTabla) {
+		var tabla = this;
+
+        apiTabla.get().$promise.then(function(response) {
+            tabla.data = response.data;
+            // team.data.image_small = team.data.image_small.replace(/amp;/g, '');
+            // team.data.image_medium = team.data.image_medium.replace(/amp;/g, '');
+            // team.data.image_large = team.data.image_large.replace(/amp;/g, '');
+            })
+        
+	}
+
+})()
--- app/factory/team.js
+++ app/factory/team.js
@@ -3,7 +3,8 @@
 
 	angular
 		.module('sopitasApp')
-		.factory('apiTeam', apiTeam);
+		.factory('apiTeam', apiTeam)
+        .factory('apiTabla', apiTabla);
 
 	apiTeam.$inject = ["$resource"];
     function apiTeam($resource){
@@ -12,5 +13,15 @@
     	var hsh = '823fa80840a88fb26ecb56eaf68250e5';
     	return $resource("http://api.talksport.com/api/team/?method=getItem&team_id=1&hsh=823fa80840a88fb26ecb56eaf68250e5&app_id=mshndevmkt");
         // return $resource("http://www.talksport.com/api/team/?method=getItem&team_id="+team_id+"&hsh="+hsh+"&app_id="+app_id);
+    }
+
+        apiTabla.$inject = ["$resource"];
+    function apiTabla($resource){
+        var team_id = 1;
+        var app_id = 'mshndevmkt';
+        var hsh = '823fa80840a88fb26ecb56eaf68250e5';
+        return $resource("http://api.talksport.com/api/tournament/?method=getTable&sport_id=1&tournament_id=8&season_id=2016&hsh=87cff7f9245a84e5e0be934c6e7b837c&app_id=mshndevmkt&format=jsonp");
+        //return $resource("http://api.talksport.com/api/team/?method=getItem&team_id=1&hsh=823fa80840a88fb26ecb56eaf68250e5&app_id=mshndevmkt");
+        //return $resource("http://www.talksport.com/api/team/?method=getItem&team_id="+team_id+"&hsh="+hsh+"&app_id="+app_id);
     }
 })()
