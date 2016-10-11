f977db49976cc49e6dd8d54ffd43456c560e7747
Manuel Comi <mcomi@hotmail.com>
2016-10-11 00:52:13 -0500

    agrego libreria js-php-unserialize para deserializar las alertas que vienen serializadas en php

--- app/component/alertas/alertas.js
+++ app/component/alertas/alertas.js
@@ -13,11 +13,19 @@
 	alertasCtrl.$inject = ['apiAlerts']
 	function alertasCtrl (apiAlerts) {
 		var alert = this;
+		alert.details = [];
 
 		apiAlerts.get().$promise.then(function(response) {
-			alert.data = response.data.alerts;
+			var data = PHPUnserialize.unserialize(response.data.alerts);
             //alert.data = response.data.alerts.replace(/;/g, ',');
-            console.log(alert.data);
+            console.log(data);
+            for(var i in data) {
+            	
+            	alert.details.push(PHPUnserialize.unserialize(data[i].detail));
+            	
+            }
+            console.log(alert.details);
+            //console.log(PHPUnserialize.unserialize(alert.data));
             //console.log(alert.data.replace(/s:/g, 's').replace(/a:/g, 'a').replace(/i:/g, 'i'));
             // team.data.image_small = team.data.image_small.replace(/amp;/g, '');
             // team.data.image_medium = team.data.image_medium.replace(/amp;/g, '');
