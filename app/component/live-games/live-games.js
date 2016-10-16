14a8700c9df3f2fb85cbb3e487ec253bfdeeffc9
Manuel Comi <mcomi@hotmail.com>
2016-10-15 23:07:10 -0500

    cambios

--- app/component/live-games/live-games.js
+++ app/component/live-games/live-games.js
@@ -19,16 +19,18 @@
 			games.live = [];
 			games.full = [];
             var data = response.data;
-            console.log(data.full);
+            
             for(var i in data){
             	if(data[i].match_status == 'LIVE'){
             		games.live.push(data[i]);
+
             	}else{
             		games.full.push(data[i]);
             	}
 
             // 	apiLivestream.get().$promise.then(function(response) {
             }
+            console.log(games.live);
     	});    
 	}
 })()
