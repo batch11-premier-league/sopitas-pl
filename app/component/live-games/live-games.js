76c9e94355c969921946703a1d7da7e21bf87983
Manuel Comi <mcomi@hotmail.com>
2016-10-18 18:26:07 -0500

    + aniado firebase para probar las notificaciones cuando cambia la base de datos

--- app/component/live-games/live-games.js
+++ app/component/live-games/live-games.js
@@ -1,70 +1,110 @@
-(function () {
-	'use strict';
+(function() {
+    'use strict';
 
-	var liveGames = {
-		templateUrl: './app/component/live-games/live-games.html',
-		controller: liveGamesCtrl
-	}
+    var liveGames = {
+        templateUrl: './app/component/live-games/live-games.html',
+        controller: liveGamesCtrl
+    }
 
-	angular
-		.module('sopitasApp')
-		.component('liveGames', liveGames);
+    angular
+        .module('sopitasApp')
+        .component('liveGames', liveGames);
 
-	liveGamesCtrl.$inject = ['apiLiveGames', 'apiLivestream','apiLastResults','apiAudioRepeat','$timeout']
-	function liveGamesCtrl (apiLiveGames, apiLivestream, apiLastResults,apiAudioRepeat,$timeout) {
-		var games = this;
-		games.live = [];
-		games.full = [];
-		games.next = [];
-		// games.playGameAudio = playGameAudio;
-		// games.PushtapePlayer = PushtapePlayer;
+    liveGamesCtrl.$inject = ['apiLiveGames', 'apiLivestream', 'apiLastResults', 'apiAudioRepeat', '$timeout','$interval']
 
-		apiLiveGames.get().$promise.then(function(response) {
-			// games.live = [];
-			// games.full = [];
-            var data = response.data;
-            
-            for(var i in data){
-            	if(data[i].match_status == 'LIVE' || data[i].match_status == 'HALF-TIME'){
-            		games.live.push(data[i]);
+    function liveGamesCtrl(apiLiveGames, apiLivestream, apiLastResults, apiAudioRepeat, $timeout, $interval) {
+        var games = this;
+        games.live = [];
+        games.full = [];
+        games.next = [];
+        games.db = null;
+        var interval = null
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyBOEd731xsw1BDs0SiP_nu8rdxQoG63DSc",
+            authDomain: "brilliant-torch-7697.firebaseapp.com",
+            databaseURL: "https://brilliant-torch-7697.firebaseio.com",
+            storageBucket: "brilliant-torch-7697.appspot.com",
+            messagingSenderId: "1030875328300"
+        };
+        firebase.initializeApp(config);
 
-            	}else if(data[i].match_status == 'PRE-MATCH'){
-            		games.next.push(data[i]);
-            	}
+        var db = firebase.database();
 
-            // 	apiLivestream.get().$promise.then(function(response) {
-            }
-            if(games.live.length > 0){
-            	for(var i in games.live){
+        function writeGamesData(games) {
+            firebase.database().ref('games/').set(games);
+        }
 
-            		apiLivestream.get({ optaId: games.live[i].opta_id }).$promise.then(function(response) {
-            			games.live[i].audio = response.data.sHttpPath;
-            		});
-            	}
-            }
-    	});
+        var alertRef = firebase.database().ref('games/0');
+        alertRef.on('value', function(snapshot) {
+            console.log(snapshot.val());
+        });
 
-    	apiLastResults.get().$promise.then(function(response) {  
-    		games.full = response.data;
-    		if(games.full.length > 0){
-    			angular.forEach(games.full , function(game){
-    				console.log(game.id);
-    				apiAudioRepeat.get({ optaId: game.opta_id }).$promise.then(function(response) {
-	            			game.audio = response.data[0].enclosure_url;
-	            			
-	            		}, function(error) {
-						    game.audio = false;
-						});
-    			})
-            	// for(var i in games.full){
-            		// console.log(games.full[i]);
-            		// console.log(games.full[i]);
-	            		
-					
-            		
-            	
-            }
-    	});  
+        getLastResults();
+        startContinuosRequestForLiveGames();
 
-	}
+        function startContinuosRequestForLiveGames () {
+        	interval = $interval(getLiveMatches, 1000);
+        }
+
+        function stopLiveRequest () {
+            if( interval !== undefined){
+                $interval.cancel(interval);
+                interval = undefined;
+                console.log('Termine el intervalo');
+            }        
+        }
+
+        var countInterval = 0
+
+        function getLiveMatches() {
+        	countInterval++
+        	console.log('Intervalo: '+countInterval)
+            apiLiveGames.get().$promise.then(function(response) {
+                angular.forEach(data, function(game) {
+                    if (game.match_status == 'LIVE' || game.match_status == 'HALF-TIME') {
+                        games.live.push(game);
+
+                    } else if (game.match_status == 'PRE-MATCH') {
+                        games.next.push(game);
+                    }
+                })
+
+                angular.forEach(games.live, function(game) {
+                    apiLivestream.get({ optaId: game.opta_id }).$promise.then(function(response) {
+                        game.audio = response.data.sHttpPath;
+                    }, function(error) {
+                        game.audio = false;
+                    });
+                })
+            }, function(error) {
+                stopLiveRequest()
+                console.log('No encontre partidos en vivo');
+            });
+        }
+
+        function getLastResults () {
+	        apiLastResults.get().$promise.then(function(response) {
+	            games.full = response.data;
+	            writeGamesData(games.full);
+	            if (games.full.length > 0) {
+	                angular.forEach(games.full, function(game) {
+	                    console.log(game.id);
+	                    apiAudioRepeat.get({ optaId: game.opta_id }).$promise.then(function(response) {
+	                        game.audio = response.data[0].enclosure_url;
+
+	                    }, function(error) {
+	                    	console.log(error);
+	                        game.audio = false;
+	                    });
+	                })
+
+	            }
+	        }, function(error) {
+                console.log('Error al buscar los últimos resultados');
+            });
+        }
+
+
+    }
 })()
--- app/factory/team.js
+++ app/factory/team.js
@@ -33,14 +33,14 @@
     apiLineup.$inject = ["$resource"];
     function apiLineup($resource){
     	var hsh = '823fa80840a88fb26ecb56eaf68250e5';// match id
-        return $resource("http://talksport.com/api/team/?method=getItems&season_id=2016&tournament_id=8&hsh=204a2b292b4f62737accf9e564a17f7e&app_id=mshndevmkt");
+        return $resource("http://talksport.com/api/team/?method=getItems&season_id=2016&tournament_id=8&hsh=204a2b292b4f62737accf9e564a17f7e&app_id=mshndevmkt&format=jsonp");
         //return $resource("http://api.talksport.com/api/team/?method=getItem&team_id=1&hsh=823fa80840a88fb26ecb56eaf68250e5&app_id=mshndevmkt");
         //return $resource("http://www.talksport.com/api/team/?method=getItem&team_id="+team_id+"&hsh="+hsh+"&app_id="+app_id);
     }
 
     apiListGames.$inject = ["$resource"];
     function apiListGames($resource){
-    	return $resource('http://talksport.com/api/fixture/?method=getList&sport_id=1&tournament_id=8&season_id=2016&hsh=255310244028f29f0d79b2581a4450cc&app_id=mshndevmkt')
+    	return $resource('http://talksport.com/api/fixture/?method=getList&sport_id=1&tournament_id=8&season_id=2016&hsh=255310244028f29f0d79b2581a4450cc&app_id=mshndevmkt&format=jsonp')
     }
 
     apiLiveGames.$inject = ["$resource"];
@@ -50,27 +50,27 @@
         var mm = date.getMonth()+1;
         var dd = date.getDate();
         var today = year+'-'+mm+'-'+dd;
-        return $resource('http://api.talksport.com/api/fixture/?method=getList&fixture_date='+today+'&sport_id=1&tournament_id=8&season_id=2016&hsh=255310244028f29f0d79b2581a4450cc&app_id=mshndevmkt')
+        return $resource('http://api.talksport.com/api/fixture/?method=getList&fixture_date='+today+'&sport_id=1&tournament_id=8&season_id=2016&hsh=255310244028f29f0d79b2581a4450cc&app_id=mshndevmkt&format=jsonp')
     }
 
     apiLastResults.$inject = ['$resource'];
     function apiLastResults($resource){
-        return $resource('http://talksport.com/api/fixture/?method=getResults&sport_id=1&tournament_id=8&season_id=2016&hsh=c178aa4d845d75c676574e370b2d8f0c&app_id=mshndevmkt&day_limit=7')
+        return $resource('http://talksport.com/api/fixture/?method=getResults&sport_id=1&tournament_id=8&season_id=2016&hsh=c178aa4d845d75c676574e370b2d8f0c&app_id=mshndevmkt&day_limit=7&format=jsonp')
     }
 
     apiLivestream.$inject = ['$resource'];
     function apiLivestream ($resource) {
-        return $resource('http://talksport.com/api/audio/?method=getLiveStream&opta_id=:optaId&lang=es&hsh=38bdf407e9915b72785b0412fc21b418&app_id=mshndevmkt', { optaId : '@_opta_id' })
+        return $resource('http://talksport.com/api/audio/?method=getLiveStream&opta_id=:optaId&lang=es&hsh=38bdf407e9915b72785b0412fc21b418&app_id=mshndevmkt&format=jsonp', { optaId : '@_opta_id' })
     }  
 
     apiAudioRepeat.$inject = ['$resource'];
     function apiAudioRepeat ($resource) {
-        return $resource('http://talksport.com/api/audio/?method=getFull&opta_id=:optaId&lang=es&hsh=9d5f6a17d1851a1080d0565171f17329&app_id=mshndevmkt', { optaId : '@_opta_id' })
+        return $resource('http://talksport.com/api/audio/?method=getFull&opta_id=:optaId&lang=es&hsh=9d5f6a17d1851a1080d0565171f17329&app_id=mshndevmkt&format=jsonp', { optaId : '@_opta_id' })
     }
 
     apiAlerts.$inject = ['$resource'];
     function apiAlerts ($resource) {
-    	return $resource('http://talksport.com/api/fixture/?method=getItem&fixture_id=58479688&hsh=823fa80840a88fb26ecb56eaf68250e5&app_id=mshndevmkt')
+    	return $resource('http://talksport.com/api/fixture/?method=getItem&fixture_id=58479688&hsh=823fa80840a88fb26ecb56eaf68250e5&app_id=mshndevmkt&format=jsonp')
     }
 
 })()
